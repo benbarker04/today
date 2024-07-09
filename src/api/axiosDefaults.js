5124e7b73bda9990f01c694c23ceb9f0d0d6e502
Ben Barker <benbarker074@gmail.com>
2024-07-09 14:16:27 +0000

    add axios interceptors

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://bens-today-project-api-0a7c2244176f.herokuapp.com/'
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create()
+export const axiosRes = axios.create()
