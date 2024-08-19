9df45831239393e5a71aceb37ce9641352d2865d
Ben Barker <benbarker074@gmail.com>
2024-08-19 19:53:12 +0000

    add and stlyed navbar for logged in users

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,10 +4,19 @@ import styles from '../App.module.css'
 import navstyles from '../styles/NavBar.module.css'
 import { NavLink } from 'react-router-dom/cjs/react-router-dom'
 import { useCurrentUser } from '../context/CurrentUserContext'
+import Avatar from './Avatar'
 
 function NavBar() {
     const currentUser = useCurrentUser()
-    const loggedInIcons = <>{currentUser?.username}</>
+    const addPostIcon = (
+        <NavLink to='/posts/create'><i class="fa-solid fa-plus"></i>Add Post</NavLink>
+    )
+    const loggedInIcons = <>
+        <NavLink to='/feed'><i class="fa-solid fa-bars-staggered"></i>Feed</NavLink>
+        <NavLink to='/liked'><i class="fa-solid fa-thumbs-up"></i>Likes</NavLink>
+        <NavLink to='/' onclick={() => {}}><i class="fa-solid fa-arrow-right-from-bracket"></i>Sign-Out</NavLink>
+        <NavLink to={`/profiles/${currentUser?.profile_id}`}>{currentUser?.username}</NavLink>
+    </>
     const loggedOutIcons = <><NavLink to='/signin'><i class="fa-solid fa-right-to-bracket"></i>Sign-In</NavLink></>
 
     return (
@@ -16,6 +25,7 @@ function NavBar() {
                 <NavLink to='/'>
                     <Navbar.Brand href="#home"><h1 className={styles.logo}>Today</h1></Navbar.Brand>
                 </NavLink>
+                {currentUser && addPostIcon}
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className='ml-auto'>
