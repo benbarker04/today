5945b9682de9d33a791d1929de15fa78fff96a78
Ben Barker <benbarker074@gmail.com>
2024-08-21 16:49:44 +0000

    add handleclickoutisde function so that the toggle menu closes when clicked outside the menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Container, Nav, Navbar } from 'react-bootstrap'
 import styles from '../App.module.css'
 import navstyles from '../styles/NavBar.module.css'
@@ -12,7 +12,19 @@ import axios from 'axios'
 function NavBar() {
     const currentUser = useCurrentUser()
     const setCurrentUser = useSetCurrentUser();
-
+    const [expanded, setExpanded] = useState(false)
+    const ref = useRef(null)
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (ref.current && !ref.current.contains(event.target)){
+                setExpanded(false)
+            }
+        }
+        document.addEventListener('mouseup', handleClickOutside)
+        return () => {
+            document.removeEventListener('mouseup', handleClickOutside)
+        }
+    },[ref])
     
 
     const handSignOut = async () => {
@@ -33,12 +45,12 @@ function NavBar() {
     const loggedOutIcons = <><NavLink to='/signin'><i class="fa-solid fa-right-to-bracket"></i>Sign-In</NavLink></>
 
     return (
-        <Navbar className={navstyles.NavBar} fixed='top' bg="light" expand="lg">
+        <Navbar expanded={expanded} className={navstyles.NavBar} fixed='top' bg="light" expand="lg">
             <Container>
                 <NavLink to='/'>
                     <Navbar.Brand href="#home"><h1 className={styles.logo}>Today</h1></Navbar.Brand>
                 </NavLink>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Toggle ref={ref} onClick={() => setExpanded(!expanded)} aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className='ml-auto'>
                         <NavLink to='/'><i class="fa-solid fa-house-chimney"></i>Home</NavLink>
